c65730cf233bfbf3a62e21bd848b732af4ad84da
ausma-tc <t.corbieres@gmail.com>
2023-05-01 23:07:10 +0200

    Adding stripe on payement service

--- audiophile-front/src/components/Cart/Cart.jsx
+++ audiophile-front/src/components/Cart/Cart.jsx
@@ -5,8 +5,8 @@ import './Cart.scss';
 import { useSelector } from "react-redux";
 import { removeItem, resetCart } from "../../redux/cartReducer";
 import { useDispatch } from "react-redux";
-// import { makeRequest } from "../../makeRequest";
-// import { loadStripe } from "@stripe/stripe-js";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from '@stripe/stripe-js';
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
@@ -20,23 +20,29 @@ const Cart = () => {
     return total.toFixed(2);
   };
 
-  // const stripePromise = loadStripe(
-  //   "pk_test_eOTMlr8usx1ctymXqrik0ls700lQCsX2UB"
-  // );
-  // const handlePayment = async () => {
-  //   try {
-  //     const stripe = await stripePromise;
-  //     const res = await makeRequest.post("/orders", {
-  //       products,
-  //     });
-  //     await stripe.redirectToCheckout({
-  //       sessionId: res.data.stripeSession.id,
-  //     });
+  const stripePromise = loadStripe(
+    "pk_test_51ITVNVJfZKgwz1piBl1OIl6Ljcer213Gp57qw21tcHz4jx3J3upN143VynpFkfjXNrCstWn7VYtuJEQhVPivdGCY00eBRepfl4"
+  );
+  const handlePayment = async () => {
+    try {
+      console.log('a');
+      const stripe = await stripePromise;
+      console.log('b');
+      console.log(products);
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      console.log('c');
+      
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+      
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
@@ -57,7 +63,7 @@ const Cart = () => {
         <span>SUBTOTAL</span>
         <span>${totalPrice()}</span>
       </div>
-      <button>PROCEED TO CHECKOUT</button>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
       <span className="reset" onClick={() => dispatch(resetCart())}>
         Reset Cart
       </span>
